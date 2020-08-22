a3eacbca0fbd61895c8a83ec33c0100900dde14c
xiaohuazhou <xiaohuazhou@500px.com>
2020-08-22 13:09:44 +0800

    add jdbctemplate sample

---
+++ jdbctemplate-sample/src/main/java/net/gittab/sample/domain/ActivityStat.java
@@ -0,0 +1,43 @@
+package net.gittab.sample.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * ActivityStat.
+ *
+ * @author xiaohua zhou
+ **/
+@Data
+@Entity
+@Accessors(chain = true)
+@Table(name = "activity_stats")
+public class ActivityStat implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long activityId;
+
+	/**
+	 * 浏览量.
+	 */
+	private Long timesViewed;
+
+	/**
+	 * 作品数.
+	 */
+	private Long worksCount;
+
+	/**
+	 * 用户数.
+	 */
+	private Long userCount;
+
+}
--- jdbctemplate-sample/src/test/java/net/gittab/sample/JdbcTemplateSampleApplicationTests.java
+++ jdbctemplate-sample/src/test/java/net/gittab/sample/JdbcTemplateSampleApplicationTests.java
@@ -2,9 +2,11 @@ package net.gittab.sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class JdbctemplateSampleApplicationTests {
+@ActiveProfiles("development")
+public class JdbcTemplateSampleApplicationTests {
 
 	@Test
 	void contextLoads() {
